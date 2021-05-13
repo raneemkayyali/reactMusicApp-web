aaebf85c1414c234d816da844d3401cf14b9f51f
Raneem Kayyali <raneemrk@hotmail.com>
2021-05-13 14:57:56 +0300

    Add project files.

---
+++ ReactMusicApp/server.js
@@ -0,0 +1,17 @@
+/*import React from "react";
+import ReactDOM from "react-dom";
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";*/
+
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    //ReactDOM.render(
+      //  <ReactJkMusicPlayer {...options} />,
+        //document.getElementById("root")
+    //);
+}).listen(port);
